aa3fb07a9937cbf042c318312fc961f5ee7551e4
shivani-senguttuvan5 <shivani.senguttuvan5@gmail.com>
2022-09-30 20:36:13 -0500

    updated refresh

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -53,10 +53,12 @@ class HomeTableViewController: UITableViewController {
         
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets:[NSDictionary]) in
             self.tweetArray.removeAll()
+            
             for tweet in tweets{
                 self.tweetArray.append(tweet)
             }
             self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
         }, failure: { (Error) in
             print("Could not retrieve tweets!")
         })
